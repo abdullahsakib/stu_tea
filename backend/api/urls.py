e32ecfe0f10f12b30f4f5c7b0c20202991ac8376
abdullahsakib <54322794+abdullahsakib@users.noreply.github.com>
2025-09-13 23:19:32 +0600

    Update urls.py

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import QuestionListCreate, AnswerCreate
+from .views import QuestionListCreateView, answer_list_create
+
 urlpatterns = [
-    path("questions/", QuestionListCreate.as_view(), name="questions"),
-    path("answers/", AnswerCreate.as_view(), name="answers"),
+    path("questions/", QuestionListCreateView.as_view(), name="question-list-create"),
+    path("questions/<int:pk>/answers/", answer_list_create, name="answer-list-create"),
 ]
