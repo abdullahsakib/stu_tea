e747cb08b77753f792a570f77350f9be4b2a6183
abdullahsakib <54322794+abdullahsakib@users.noreply.github.com>
2025-09-13 23:19:01 +0600

    Update serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,11 +1,16 @@
 from rest_framework import serializers
 from .models import Question, Answer
+
+
 class AnswerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Answer
-        fields = ['id','question','student_name','text','created_at']
+        fields = ["id", "text", "created_at"]
+
+
 class QuestionSerializer(serializers.ModelSerializer):
     answers = AnswerSerializer(many=True, read_only=True)
+
     class Meta:
         model = Question
-        fields = ['id','text','created_at','answers']
+        fields = ["id", "text", "created_at", "answers"]
