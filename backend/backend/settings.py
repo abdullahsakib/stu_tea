f83100e5c88dd5126b608e015f20e22c8c9726cf
abdullahsakib <54322794+abdullahsakib@users.noreply.github.com>
2025-09-14 20:46:27 +0600

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "dev-secret"
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     "django.contrib.admin",
